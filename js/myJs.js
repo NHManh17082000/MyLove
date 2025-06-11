8e61be603835411a5153d35f2696e5511913d4dc
NHManh17082000 <nguyenhuumanh1708@gmail.con>
2025-06-11 16:22:05 +0700

    my love

---
+++ js/myJs.js
@@ -0,0 +1,92 @@
+const textConfig = {
+  text1: "He luu em!",
+  text2: "Anh có điều này muốn nói với em.",
+  text3: "❤️❤️❤️❤️Anh yêu em❤️❤️❤️❤️",
+  text4: "Em có thể đồng ý trở thành người yêu anh được không?🥰",
+  text5: "Anh mơ àà???",
+  text6: "Em yêu anh quá trời <3",
+};
+
+$(document).ready(function () {
+  // process bar
+  setTimeout(function () {
+    firstQuestion();
+    $(".spinner").fadeOut();
+    $("#preloader").delay(350).fadeOut("slow");
+    $("body").delay(350).css({
+      overflow: "visible",
+    });
+  }, 600);
+
+  $("#text3").html(textConfig.text3);
+  $("#text4").html(textConfig.text4);
+  $("#no").html(textConfig.text5);
+  $("#yes").html(textConfig.text6);
+
+  function firstQuestion() {
+    $(".content").hide();
+    Swal.fire({
+      title: textConfig.text1,
+      text: textConfig.text2,
+      imageUrl: "img/cuteCat.jpg",
+      imageWidth: 300,
+      imageHeight: 300,
+      background: '#fff url("img/iput-bg.jpg")',
+      imageAlt: "Custom image",
+    }).then(function () {
+      $(".content").show(200);
+    });
+  }
+
+  // switch button position
+  function switchButton() {
+    var audio = new Audio("sound/duck.mp3");
+    audio.play();
+    var leftNo = $("#no").css("left");
+    var topNO = $("#no").css("top");
+    var leftY = $("#yes").css("left");
+    var topY = $("#yes").css("top");
+    $("#no").css("left", leftY);
+    $("#no").css("top", topY);
+    $("#yes").css("left", leftNo);
+    $("#yes").css("top", topNO);
+  }
+  // move random button position
+  function moveButton() {
+    var audio = new Audio("sound/Swish1.mp3");
+    audio.play();
+    if (screen.width <= 600) {
+      var x = Math.random() * 300;
+      var y = Math.random() * 500;
+    } else {
+      var x = Math.random() * 500;
+      var y = Math.random() * 500;
+    }
+    var left = x + "px";
+    var top = y + "px";
+    $("#no").css("left", left);
+    $("#no").css("top", top);
+  }
+
+  var n = 0;
+  $("#no").mousemove(function () {
+    if (n < 1) switchButton();
+    if (n > 1) moveButton();
+    n++;
+  });
+  $("#no").click(() => {
+    if (screen.width >= 900) switchButton();
+  });
+
+
+  // change to heart page
+  document.getElementById("yes").addEventListener("click", function () {
+  window.location.href = "Heart.html";
+  });
+  //play music
+  document.addEventListener("click", function playOnce() {
+  const audio = document.getElementById("bgm");
+  audio.play();
+  document.removeEventListener("click", playOnce); // chỉ phát 1 lần
+  });
+});
